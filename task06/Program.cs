af707e0cfeb0f07897c9f37704dafbf156bdf7ba
Vasy <taki003@yandex.ru>
2023-10-06 22:39:51 +0700

    задача 6

---
+++ task06/Program.cs
@@ -0,0 +1,13 @@
+﻿
+
+
+System.Console.WriteLine("задай 1 число");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number % 2 == 0)
+{
+    System.Console.WriteLine($"число {number} четное");
+}
+else
+{
+    System.Console.WriteLine($"число {number} нечетное");
+}
