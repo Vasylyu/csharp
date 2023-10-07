9975bb8d24f0c4d1fdf0b98902c570ef5c4bbd06
Vasy <taki003@yandex.ru>
2023-10-08 02:00:18 +0700

    6task

--- task06/Program.cs
+++ task06/Program.cs
@@ -1,5 +1,7 @@
-﻿
-
+﻿/*Внутри класса Answer напишите метод CheckIfEven, 
+который на вход принимает число number и выводит, 
+является ли число чётным (делится ли оно на два без остатка).
+*/
 
 System.Console.WriteLine("задай 1 число");
 int number = Convert.ToInt32(Console.ReadLine());
