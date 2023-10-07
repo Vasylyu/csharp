c8db532a899184885ebd746b8e3308e027142a96
Vasy <taki003@yandex.ru>
2023-10-08 01:51:35 +0700

    7task

---
+++ task07/Program.cs
@@ -0,0 +1,8 @@
+﻿/* Напишите программу, которая на входе принимает трехзначенное число
+и на выходе показывает последнию цифру этого числа.
+*/
+
+System.Console.WriteLine("Введите число");
+int userNumber1 = Convert.ToInt32(Console.ReadLine());
+int result = userNumber1 % 10;
+System.Console.WriteLine(result);
