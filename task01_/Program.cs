1210f4759025c309489b0c8f676529b3b38f631f
Vasy <taki003@yandex.ru>
2023-10-08 01:54:00 +0700

    1task

---
+++ task01_/Program.cs
@@ -0,0 +1,18 @@
+﻿/* 
+Напишите программу, которая принимает два числа и проверяет,
+является ли первое число квадратом второго
+*/
+Console.WriteLine("Введите первое число: ");
+
+int userNumber1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите второе число: ");
+
+int userNumber2 = Convert.ToInt32(Console.ReadLine());
+
+
+int sqNumber2 = userNumber2 * userNumber2;
+
+bool result = userNumber1 == sqNumber2;
+
+Console.WriteLine(result);
