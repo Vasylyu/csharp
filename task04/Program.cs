21fbc0aa8629fc0dcf4696c4a90dbffea2b802df
Vasy <taki003@yandex.ru>
2023-10-08 13:08:03 +0700

    4task

---
+++ task04/Program.cs
@@ -0,0 +1,19 @@
+﻿/*
+Внутри класса Answer напишите метод FindMax, который принимает на вход три числа
+ и выдаёт максимальное из этих чисел.
+*/
+System.Console.WriteLine("Задай 3 числа");
+int numbera = Convert.ToInt32(Console.ReadLine());
+int numberb = Convert.ToInt32(Console.ReadLine());
+int numberc = Convert.ToInt32(Console.ReadLine());
+int max = numbera;
+if (numberb > max)
+{
+    max = numberb;
+}
+
+if (numberc > max)
+{
+    max = numberc;
+}
+System.Console.WriteLine("Максимальное число " + max);
