a100e55b7e8f782bd4645484de4e1f43bf24deac
Vasy <taki003@yandex.ru>
2023-10-15 15:54:01 +0700

    task22

--- task22/Program.cs
+++ task22/Program.cs
@@ -10,5 +10,6 @@ System.Console.Write("Введите число: ");
 int n = Convert.ToInt32(Console.ReadLine());
 
 for (int i = 1; i <= n; i++)
-
-    System.Console.Write(Math.Pow(i, 2) + " ");
+{
+    System.Console.WriteLine($" {i} * {i} = {Math.Pow(i, 2)} ");
+}
