bec31618475bc51497a593aac3df62ad2dbbc9bf
Vasy <taki003@yandex.ru>
2023-10-16 16:47:51 +0700

    new task30

---
+++ task26/Program.cs
@@ -0,0 +1,21 @@
+﻿/*
+ Напишите программу, которая принимает на вход число и выдаёт количество цифр в числе.
+456 -> 3
+78 -> 2
+89126 -> 5
+*/
+System.Console.Write("Введите число A: ");
+int userA = Convert.ToInt32(Console.ReadLine());
+
+int Number(int a)
+{
+    int num = 0;
+
+    for (int i = a; i > 0; i = i / 10)
+    {
+         num++;
+    }
+    return num;
+}
+int userResult = Number(userA);
+System.Console.WriteLine(userResult);
---
+++ task28/Program.cs
@@ -0,0 +1,21 @@
+﻿/*
+Напишите программу, которая принимает на вход число N 
+и выдаёт произведение чисел от 1 до N.
+4 -> 24
+5 -> 120
+*/
+System.Console.Write("Введите число N: ");
+int userN = Convert.ToInt32(Console.ReadLine());
+
+int Number(int n)
+{
+    int num = 1;
+
+    for (int i = 1; i <= n;  i++)
+    {
+         num = num * i;
+    }
+    return num;
+}
+int userResult = Number(userN);
+System.Console.WriteLine(userResult);
