37fb17b61716b4e21a0f4e2262372a3044b644ae
Vasy <taki003@yandex.ru>
2023-10-20 21:52:52 +0700

    task37

--- task29/Program.cs
+++ task29/Program.cs
@@ -1,41 +1,11 @@
 ﻿/*
-Напишите программу, которая берет массив у пользователя, его размери выводит их на экран.
+Напишите программу, которая берет массив у пользователя, его размер и выводит их на экран.
 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
 6, 1, 33 -> [6, 1, 33]
 */
-/*Console.Clear();
 
-System.Console.Write("Задай диапозон массива от: ");
-int[] userStart = Convert.ToInt32(Console.ReadLine());
-System.Console.Write("Задай диапозон массива до: ");
-int[] userEnd = Convert.ToInt32(Console.ReadLine());
-System.Console.Write("Задай разmер массива: ");
-int[] userSize = Convert.ToInt32(Console.ReadLine());
 
-int[] GetArray(int star, int stop, int size);
-{
-    int[] ray = new int[size];
-    for (int i = 0; i < size; i++)
-    {
-        ray[i] = new Random().Next(start, stop + 1);
-    }
-    return ray;
-}
-void PrintArray(int[] arr)
-{
-    System.Console.Write("[");
-    for (int i = 0; i<arr.length; i++)
-    {
-        System.Console.Write(arr[i]);
-        if (i < arr.length -1)
-        System.Console.WriteLine(", ");
-    }
-    System.Console.Write("]");
-}
 
-int[] userArray = GetArray(userStart, userEnd, userSize);
-PrintArray(userArray);
-*/
 int[] GetArray(int start, int end, int size)
 {
     int[] array = new int[size];
@@ -53,7 +23,7 @@ void PrintArray(int[] arr)
     {
         System.Console.Write(arr[i]);
         if (i < arr.Length - 1)
-        System.Console.Write(", ");//[5, 7, 1, 4]
+        System.Console.Write(", ");
     }
     System.Console.Write("]");
 }
@@ -61,9 +31,9 @@ void PrintArray(int[] arr)
 System.Console.WriteLine("Задай диапазон массива от:");
 int userStart = Convert.ToInt32(Console.ReadLine());
 System.Console.WriteLine("Задай диапазон массива до:");
-int userEnd = Convert.ToInt32(Console.ReadLine());
+int userStop = Convert.ToInt32(Console.ReadLine());
 System.Console.WriteLine("Задай размер массива:");
 int userSize = Convert.ToInt32(Console.ReadLine());
 
-int[] userArray = GetArray(userStart, userEnd, userSize);
+int[] userArray = GetArray(userStart, userStop, userSize);
 PrintArray(userArray);
