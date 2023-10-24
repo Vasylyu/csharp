4380b66cf6cf5b7b34010cb188c6b3a46e614bbd
Vasy <taki003@yandex.ru>
2023-10-25 02:41:15 +0700

    task45

--- task44/Program.cs
+++ task44/Program.cs
@@ -4,11 +4,43 @@
 Если N = 3 -> 0 1 1
 Если N = 7 -> 0 1 1 2 3 5 8
 */
+Console.Write("Введите количество чисел Фибоначчи: ");
+int N = int.Parse(Console.ReadLine());
 
-int TakeEnteredNumber(string message)
+if (N < 0)
 {
-    System.Console.WriteLine(message);
-    int result = Convert.ToInt32(Console.ReadLine());
-    return result;
+    Console.WriteLine("Пожалуйста, введите положительное число.");
+    return;
 }
-int userNumber = TakeEnteredNumber("Введите число ")
+
+if (N == 0)
+{
+    Console.WriteLine("Если " + N + " число Фибоначчи: 1");
+    return;
+}
+
+int first = 0;
+int second = 1;
+
+Console.Write("Если " + N + " число Фибоначчи: ");
+
+if (N >= 1)
+{
+    Console.Write(first);
+}
+
+if (N >= 2)
+{
+    Console.Write(", " + second);
+}
+
+for (int i = 3; i <= N; i++)
+{
+    int next = first + second;
+    Console.Write(", " + next);
+    first = second;
+    second = next;
+}
+
+Console.WriteLine();
+
