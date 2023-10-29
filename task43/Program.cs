12a14c140efbb156a67879954c2eb651967643c7
Vasy <taki003@yandex.ru>
2023-10-30 00:54:55 +0700

    task52 исправлено

--- task43/Program.cs
+++ task43/Program.cs
@@ -4,7 +4,7 @@
 
 b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
 */
-/*
+
 System.Console.WriteLine("введите значение b1");
 double b1 = Convert.ToInt32(Console.ReadLine());
 System.Console.WriteLine("введите значение k1");
@@ -25,37 +25,9 @@ void CrossLine(double b1, double k1, double b2, double k2)
             System.Console.WriteLine("Прямые параллельны");
         return;
     }
-
-
-    double x = (-b1 + b1) / (-k1 + k2);
+    double x = (-b2 + b1) / (-k1 + k2);
     double y = k2 * x + b2;
     System.Console.WriteLine($"Две прямые пересекаются в точке с координатами X: {x}, Y: {y}");
 }
 
-CrossLine(b1, k1, b2, k2);
-*/
-Console.WriteLine("введите значение b1");
-double b1 = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("введите число k1");
-double k1 = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("введите значение b2");
-double b2 = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("введите число k2");
-double k2 = Convert.ToInt32(Console.ReadLine());
-
-void CrossLine(double b1, double k1, double b2, double k2)
-{
-
-    if (k1 == k2)
-    {
-        if (b1 == b2)
-            System.Console.WriteLine("Прямые совпадают");
-        else
-            System.Console.WriteLine("Прямые параллельны");
-            return;
-    }
-    double x = (-b2 + b1) / (-k1 + k2);
-    double y = k2 * x + b2;
-    Console.WriteLine($"Две прямые пересекаются в точке с координатами X: {x}, Y: {y}");
-}
 CrossLine(b1, k1, b2, k2);
