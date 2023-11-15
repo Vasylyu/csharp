c05127c35c379381e5d8f052d5da667449f5f6af
Vasy <taki003@yandex.ru>
2023-11-15 14:05:35 +0700

    task55

--- task57/Program.cs
+++ task57/Program.cs
@@ -1,16 +1,21 @@
 ﻿/*
-55. Задайте двумерный массив. Напишите программу, которая заменяет строки на столбцы. 
-В случае, если это невозможно, программа должна вывести сообщение для пользователя.
-Например, задан массив:
-1 4 7 2
-5 9 2 3
-8 4 2 4
-5 2 6 7
-В итоге получается вот такой массив:
-1 5 8 5
-4 9 4 2
-7 2 2 6
-2 3 4 7
+ Составить частотный словарь элементов двумерного массива. Частотный словарь содержит информацию о том,
+сколько раз встречается элемент входных данных.
+{ 1, 9, 9, 0, 2, 8, 0, 9 }
+0 встречается 2 раза
+1 встречается 1 раз
+2 встречается 1 раз
+8 встречается 1 раз
+9 встречается 3 раза
+
+1, 2, 3
+4, 6, 1
+2, 1, 6
+1 встречается 3 раза
+2 встречается 2 раза
+3 встречается 1 раз
+4 встречается 1 раз
+6 встречается 2 раза
 */
 
 int TakeEnterNumber(string message)
@@ -54,60 +59,40 @@ void Print2DArray(int[,] arr)
         System.Console.WriteLine();
     }
 }
-int[,] CopyArray(int[,] array)
-{
-    int[,] result = new int[array.GetLength(0), array.GetLength(1)];
 
+void PrintCount(int[,]array)
+{
+    for (int k = 0; k <= FindMax(array); k++)
+    {
+        int count = 0;
     for (int i = 0; i < array.GetLength(0); i++)
     {
         for (int j = 0; j < array.GetLength(1); j++)
         {
-            result[i, j] = array[i, j];
+            
+            if (k == array[i,j]) count ++;
+            
         }
     }
-    return result;
-}
-
-int[,] ChangeRows(int[,] array)
-{
-    int temperary = 0;
-    int[,] result = new int[array.GetLength(0), array.GetLength(1)];
-    result = CopyArray(array);
-    for (int j = 0; j < array.GetLength(1); j++)
-    {
-        temperary = result[0, j];
-        result[0, j] = result[array.GetLength(0) - 1, j];
-        result[array.GetLength(0) - 1, j] = temperary;
+   System.Console.WriteLine($"Число {k} встречается {count} раз");
     }
-    return result;
 }
 
-int[,] ReplaceRowsColumns(int[,] array)
+int FindMax(int [,] array)
 {
-    int[,] result = CopyArray(array);
-    int temporary = 0;
+    int max = 0;
     for (int i = 0; i < array.GetLength(0); i++)
     {
-        for (int j = 0; j < i; j++)
+        for (int j = 0; j < array.GetLength(1); j++)
         {
-            if (i != j)
-            {
-                temporary = result[i, j];
-                result[i, j] = result[j, i];
-                result[j, i] = temporary;
-            }
+            if(array[i,j] > max) max = array[i,j];
         }
-
     }
-    return result;
+    return max;
 }
+
 int userArrayRow = TakeEnterNumber("Введите количестов строк: ");
 int userArrayColumn = TakeEnterNumber("Введите количестов столбцов: ");
-if (userArrayRow != userArrayColumn)
-{
-    System.Console.WriteLine("Количество строк и столбцов не совпадает");
-   
-}
 
 int userArrayStart = TakeEnterNumber("Введите начало диапозона:");
 int userArrayEnd = TakeEnterNumber("Введите конец диапозона:");
@@ -115,9 +100,4 @@ int userArrayEnd = TakeEnterNumber("Введите конец диапозона
 int[,] user2DArray = GetRandom2DArray(userArrayStart, userArrayEnd, userArrayRow, userArrayColumn);
 
 Print2DArray(user2DArray);
-int[,] userArray = ReplaceRowsColumns(user2DArray);
-System.Console.WriteLine();
-Print2DArray(userArray);
-
-
-
+PrintCount(user2DArray);
--- task59/Program.cs
+++ task59/Program.cs
@@ -1,16 +1,17 @@
 ﻿/*
-55. Задайте двумерный массив. Напишите программу, которая заменяет строки на столбцы. 
-В случае, если это невозможно, программа должна вывести сообщение для пользователя.
+Задайте двумерный массив из целых чисел.
+Напишите программу, которая удалит строку и столбец, на
+пересечении которых расположен наименьший элемент
+массива.
 Например, задан массив:
 1 4 7 2
 5 9 2 3
 8 4 2 4
 5 2 6 7
-В итоге получается вот такой массив:
-1 5 8 5
-4 9 4 2
-7 2 2 6
-2 3 4 7
+Наименьший элемент - 1, на выходе получим следующий массив:
+9 2 3
+4 2 4
+2 6 7
 */
 
 int TakeEnterNumber(string message)
@@ -54,70 +55,57 @@ void Print2DArray(int[,] arr)
         System.Console.WriteLine();
     }
 }
-int[,] CopyArray(int[,] array)
-{
-    int[,] result = new int[array.GetLength(0), array.GetLength(1)];
 
+
+
+int[] FindMinIndex(int[,] array)
+{
+    int[] delet = { 0, 0 };
+    int min = 0;
     for (int i = 0; i < array.GetLength(0); i++)
     {
         for (int j = 0; j < array.GetLength(1); j++)
         {
-            result[i, j] = array[i, j];
-        }
-    }
-    return result;
-}
-
-int[,] ChangeRows(int[,] array)
-{
-    int temperary = 0;
-    int[,] result = new int[array.GetLength(0), array.GetLength(1)];
-    result = CopyArray(array);
-    for (int j = 0; j < array.GetLength(1); j++)
-    {
-        temperary = result[0, j];
-        result[0, j] = result[array.GetLength(0) - 1, j];
-        result[array.GetLength(0) - 1, j] = temperary;
-    }
-    return result;
-}
-
-int[,] ReplaceRowsColumns(int[,] array)
-{
-    int[,] result = CopyArray(array);
-    int temporary = 0;
-    for (int i = 0; i < array.GetLength(0); i++)
-    {
-        for (int j = 0; j < i; j++)
-        {
-            if (i != j)
+            if (array[i, j] < min)
             {
-                temporary = result[i, j];
-                result[i, j] = result[j, i];
-                result[j, i] = temporary;
+                delet[0] = i;
+                delet[1] = j;
             }
         }
+    }
+    return delet;
+}
 
+
+int[,] DeletMinColumn(int[,] array, int row, int column)
+{
+    int[,] result = new int[array.GetLength(0) - 1, array.GetLength(1) - 1];
+    for (int i = 0; i < result.GetLength(0); i++)
+    {
+        if (i != row)
+        {
+            for (int j = 0; j < result.GetLength(1); j++)
+            {
+                if (j != column)
+                {
+                    result[i, j] = array[i, j];
+                }
+            }
+        }
     }
     return result;
 }
 int userArrayRow = TakeEnterNumber("Введите количестов строк: ");
 int userArrayColumn = TakeEnterNumber("Введите количестов столбцов: ");
-if (userArrayRow != userArrayColumn)
-{
-    System.Console.WriteLine("Количество строк и столбцов не совпадает");
-   
-}
-
 int userArrayStart = TakeEnterNumber("Введите начало диапозона:");
 int userArrayEnd = TakeEnterNumber("Введите конец диапозона:");
-
 int[,] user2DArray = GetRandom2DArray(userArrayStart, userArrayEnd, userArrayRow, userArrayColumn);
 
 Print2DArray(user2DArray);
-int[,] userArray = ReplaceRowsColumns(user2DArray);
+int[] userMinIndex = FindMinIndex(user2DArray);
+int minRow = userMinIndex[0];
+int minColumn = userMinIndex[1];
+
+int[,] userDeletArray = DeletMinColumn(user2DArray, minRow, minColumn);
 System.Console.WriteLine();
-Print2DArray(userArray);
-
-
-
+Print2DArray(userDeletArray);
